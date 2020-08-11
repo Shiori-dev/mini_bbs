7372d71453490681ee7a5ec618233df50822d065
(Shiori Koimizu) <koimizu.shiori@gmail.com>
2020-08-11 11:29:53 +0900

    check.phpで「書き直す」を選択した際に記入内容を残したまま表記する処理を追加

--- join/index.php
+++ join/index.php
@@ -1,27 +1,32 @@
 <?php
+session_start();
+
 if(!empty($_POST)){
-	if($_POST['name'] ===""){
+	if($_POST['name'] ===''){
 		$error['name'] = 'blank';
 	}
-	if($_POST['email'] ===""){
+	if($_POST['email'] ===''){
 		$error['email'] = 'blank';
 	}
 	if(strlen($_POST['password']) < 4 ){
 		$error['password'] = 'length';
 	}
-	if($_POST['password'] ===""){
+	if($_POST['password'] ===''){
 		$error['password'] = 'blank';
 	}
 
 	if(empty($error)){
-		$_SESSION['JOIN'] = $_POST;
+		$_SESSION['join'] = $_POST;
 		header('Location: check.php');
 		exit();
 	}
 }
+
+if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
+		$_POST = $_SESSION['join'];
+}
+
 ?>
-
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
