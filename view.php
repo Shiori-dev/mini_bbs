b2a601de44a1ee09a6f07a1976cad53de0cdd212
(Shiori Koimizu) <koimizu.shiori@gmail.com>
2020-08-12 17:09:33 +0900

    個別に表示したいメッセージのデータを取得する処理を追加

--- view.php
+++ view.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if(empty($_REQUEST['id'])){ //idが空だったっら
+  header('Location: index.php');  //強制的にトップページへ遷移
+  exit();
+}
+//DBから表示させたいメッセージを取得
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+$posts->execute(array($_REQUEST['id']));  //URLパラメータから取得されたidを使ってメッセージを1件表示
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -16,14 +29,17 @@
   </div>
   <div id="content">
   <p>&laquo;<a href="index.php">一覧にもどる</a></p>
-
+  <!-- メッセージが表示されたときの処理 画像、名前、時間を取得して表示-->
+  <?php if($post = $posts->fetch()): ?>
     <div class="msg">
-    <img src="member_picture/" />
-    <p><span class="name">（）</span></p>
-    <p class="day"></p>
+    <img src="member_picture/<?php print(htmlspecialchars($post['picture'])); ?>" />
+    <p><?php print(htmlspecialchars($post['message'])); ?><span class="name">（<?php print(htmlspecialchars($post['name'])); ?>）</span></p>
+    <p class="day"><?php print(htmlspecialchars($post['created'])); ?></p>
     </div>
-
-	<p>その投稿は削除されたか、URLが間違えています</p>
+  <!-- メッセージの表示がエラーになったときの処理 -->
+  <?php else: ?>
+  <p>その投稿は削除されたか、URLが間違えています</p>
+  <?php endif; ?>
   </div>
 </div>
 </body>
