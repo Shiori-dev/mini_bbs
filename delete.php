d4e43cf3ff5c9e06a524eb291b5663dfeadbe472
(Shiori Koimizu) <koimizu.shiori@gmail.com>
2020-08-26 14:12:35 +0900

    ファイル名変更（ディレクトリ移動）

---
+++ delete.php
@@ -0,0 +1,29 @@
+<?php
+//セッション開始
+session_start();
+//外部ファイル読み込み
+require('dbconnect.php');
+include('template/delete.html');
+
+//ログインしているユーザーのメッセージが選択されているか確認
+if(isset($_SESSION['id'])){
+  $id = $_REQUEST['id'];
+  //DBから削除する候補のメッセージを取得(URLパラメータから取得したidを取得してSQLを走らせる)
+  $messages = $db -> prepare('SELECT * FROM posts WHERE id=?');
+  //id=?にURLパラメータから渡されたidを付与
+  $messages->execute(array($id));
+  //データを取得
+  $message = $messages->fetch();
+
+//DBから取得したmember_idとセッションに記録されたidが一致していたら削除
+  if($message['member_id'] == $_SESSION['id']){
+      //削除するSQL
+      $del = $db->prepare('DELETE FROM posts WHERE id=?');
+      //DBのデータを削除
+      $del->execute(array($id));
+  }
+}
+// 正しく削除が行われたらmain.phpに遷移
+header('Location: main.php');
+exit();
+?>
