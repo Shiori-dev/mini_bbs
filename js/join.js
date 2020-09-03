2a0d1f818a8011c85286be8ab891cfd6ae30da0f
(Shiori Koimizu) <koimizu.shiori@gmail.com>
2020-09-03 18:43:26 +0900

    ファイルの拡張子チェックを追加 必須項目の記入処理を一部変更

--- js/join.js
+++ js/join.js
@@ -18,13 +18,6 @@
   //small要素を定数に設定
   const smallNode = document.querySelectorAll('small');
 
-  //記入されたニックネームを定数nameに代入
-  const name = form.name.value;
-  //記入されたメールアドレスを定数emailに代入
-  const email = form.email.value;
-  //記入されたパスワードを定数passwordに代入
-  const password = form.password.value;
-
   //nameのチェック
   //formの入力時にイベントを設定
   formNode[0].addEventListener('input', e =>{
@@ -39,25 +32,24 @@
 
   //文字数によってメッセージを分岐
   if(name.length > 2 && name.length < 13){
-    smallNode[0].textContent = 'ユーザー名は有効です';
-  } else{
+    smallNode[0].textContent = '';
+  }else{
     smallNode[0].textContent = 'ニックネームは3~12文字で記入してください';
   }
   });
 
   //emailのチェック
   //formの入力完了時にイベントを設定
-  formNode[1].addEventListener('submit', e =>{
-  //デフォルトのイベントキャンセル
-  e.preventDefault();
+  // formNode[1].addEventListener('submit', e =>{
+  // //デフォルトのイベントキャンセル
+  // e.preventDefault();
 
-    //記入されたメールアドレスを定数emailに代入
-    const email = form.email.value;
+  //   //記入されたメールアドレスを定数emailに代入
+  //   const email = form.email.value;
 
-    //登録の重複をチェック
+  //   //登録の重複をチェック
 
-    });
-
+  //   });
 
 //passwordのチェック
   //formの入力時にイベントを設定
@@ -72,31 +64,51 @@
     //文字数によってメッセージを分岐
     if(password.length < 4 ){
       smallNode[2].textContent = 'パスワードは4文字以上で記入してください';
-    } else {
-      smallNode[2].textContent = 'パスワードは有効です';
+      return;
+    }
+  });
+
+  formNode[3].addEventListener('change' , e =>{
+   //デフォルトのイベントキャンセル
+    e.preventDefault();
+
+  // 画像ファイルを定数fileNameに代入
+  const image = document.getElementById('image').value;
+  //アップロードを許可する拡張子を設定、imageの拡張子を取得して一致しない場合の処理
+  if (!image.toUpperCase().match(/\.(jpg|jpeg|png|gif)$/i)){
+    //「ファイル選択」下にエラーメッセージを表示
+    smallNode[3].textContent ='「.jpg」「.jpeg」「.png」または「.gif」の画像を指定してください';
   }
   });
 
-
   //nameの入力有無チェックを設定
   const nameVld = function(){
+    //記入されたニックネームを定数nameに代入
+    const name = form.name.value;
     if(name === ""){
       event.preventDefault();
       smallNode[0].textContent='ニックネームを入力してください';
+      return;
     }
   }
   //emailの入力有無チェックを設定
   const emailVld = function(){
+    //記入されたメールアドレスを定数emailに代入
+    const email = form.email.value;
     if(email === ""){
       event.preventDefault();
       smallNode[1].textContent='メールアドレスを入力してください';
+      return;
     }
   }
   //passwordの入力有無チェックを設定
   const passwordVld = function(){
+    //記入されたパスワードを定数passwordに代入
+    const password = form.password.value;
     if(password === ""){
       event.preventDefault();
       smallNode[2].textContent='パスワードを入力してください';
+      return;
     }
   }
   //ボタンが押されたとき、各入力有無のチェックを実行
