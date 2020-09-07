e8f1bc88d5f5a23b7b54735519a4f66ca2b60b1b
(Shiori Koimizu) <koimizu.shiori@gmail.com>
2020-09-07 17:06:51 +0900

    ニックネームの文字数チェックを追加 エラメッセージの書き出しを変更

--- join.php
+++ join.php
@@ -1,18 +1,26 @@
-<!-- 会員登録画面 -->
 <?php
+//エラー表示
+ini_set( 'display_errors', 1 );
+error_reporting(E_ALL & ~E_NOTICE);
 //セッション開始
 session_start();
-//外部ファイル読み込み
+//dbconnect.php読み込み
 require('dbconnect.php');
+//function.php読み込み
 require('app/functions.php');
+//join.php読み込み
 include 'template/join.html';
 
-// //フォーム送信時、$_POSTが空ではない場合実行するエラーチェックを設定
+// //フォーム送信時、$_POSTで受け取った値が空ではない場合実行するエラーチェックを設定
 if(!empty($_POST)){
 	// ニックネームの記入漏れチェック
 	if($_POST['name'] ===''){
 		$error['name'] = 'blank';
 	}
+		//ニックネームの文字数チェック
+		if(strlen($_POST['name']) < 2 && ($_POST['name']) > 12  ){
+			$error['name'] = 'length';
+		}
 	//メールアドレスの記入漏れチェック
 	if($_POST['email'] ===''){
 		$error['email'] = 'blank';
@@ -25,6 +33,7 @@ if(!empty($_POST)){
 	if($_POST['password'] ===''){
 		$error['password'] = 'blank';
 	}
+	//画像がアップロードされていた場合、下記を実行
 	//画像ファイルのエラーチェック
 	$fileName = $_FILES['image']['name'];
 	if(!empty($fileName)){
@@ -34,7 +43,6 @@ if(!empty($_POST)){
 			$error['image'] ='type';
 		}
 	}
-
 
 	//アカウントの重複をチェック
 	if(empty($error)){
@@ -47,13 +55,18 @@ if(!empty($_POST)){
 		}
 	}
 
+	var_dump($_POST['name']);
+	var_dump($_POST['email']);
+	var_dump($_POST['password']);
+	var_dump($_POST['image']);
+
 //ファイルアップロード
 	if(empty($error)){
-		//日付を付加したファイル名を$imageに代入
+		//日付を付加したファイル名を作成し$imageに代入
 		$image = date('YmdHis') . $_FILES['image']['name'];
 		//保存する場所を指定してファイルをアップロード
 		move_uploaded_file($_FILES['image']['tmp_name'],'member_picture/' . $image);
-		//セションに値を保存
+		//DBに保管するためセションjoinに値を保存
 		$_SESSION['join'] = $_POST;
 		$_SESSION['join']['image'] = $image;
 		//記入内容に問題がないとき、check.phpへ遷移
@@ -62,44 +75,56 @@ if(!empty($_POST)){
 	}
 }
 
-//URLパラメータにrewriteがあれば、$_POSTに$_SESSIONの内容を代入(check.phpから戻ってきた場合)
-if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
-		$_POST = $_SESSION['join'];
-}
 
 //入力フォームのエラー文表示
 //ニックネームが記入されていない場合
 if($error['name'] === 'blank'){
-	echo 'ニックネームを入力してください';
+	$error[0]= json_encode('ニックネームを入力してください', JSON_UNESCAPED_UNICODE);
+	// $name= h(json_encode($_POST['name'], JSON_UNESCAPED_UNICODE));
 	exit();
 }
 
-//メールアドレス
+//ニックネームの文字数が規定外の場合
+if($error['name'] === 'length'){
+	$error[0]= json_encode('ニックネームは3~12文字で記入してください', JSON_UNESCAPED_UNICODE);
+	exit();
+}
+
+//メールアドレスが記入されていない場合
 if($error['email'] === 'blank'){
-		echo 'メールアドレスを入力してください';
+	$error[1] = json_encode('メールアドレスを入力してください');
 	exit();
 }
 
-//登録済みのアドレスだった場合のエラー文表記のページに遷移
+//登録済みのアドレスだった場合
 if($error['email'] === 'duplicate'){
-	echo '指定されたメールアドレスは、すでに登録されています';
+	$error[1] = json_encode('指定されたメールアドレスは、すでに登録されています');
 	exit();
 }
 
-//パスワードのエラー文表記のページに遷移
+//パスワードが4文字より少なかった場合
 if($error['password'] === 'length'){
-	echo 'パスワードは4文字以上で入力してください';
+	$error[2] = json_encode('パスワードは4文字以上で入力してください');
 	exit();
 }
 
-//パスワード記入漏れのエラー文表記のページに遷移
+//パスワード記入漏れの場合
 if($error['password'] === 'blank'){
-	echo 'パスワードを入力してください';
+	$error[2] = json_encode('パスワードを入力してください', JSON_UNESCAPED_UNICODE);
 	exit();
 }
 
-//画像ファイルの拡張子エラー文表記のページに遷移
+//画像ファイルの拡張子エラー文
 if($error['image'] ==='type'){
-	echo '「.gif」または「.jpg」または「.png」の画像を指定してください';
+	// print(json_encode('「.gif」または「.jpg」または「.png」の画像を指定してください'));
+	$error[3] = json_encode('「.gif」または「.jpg」または「.png」の画像を指定してください');
 	exit();
+}
+
+return $error;
+
+
+//URLパラメータにrewriteがあれば、$_POSTに$_SESSIONの内容を代入(check.phpから戻ってきた場合)
+if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
+	$_POST = $_SESSION['join'];
 }
