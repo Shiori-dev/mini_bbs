f02dbb6093b84827f3722a2b7bad33c66c3e9bd0
(Shiori Koimizu) <koimizu.shiori@gmail.com>
2020-09-11 21:42:05 +0900

    jQueryで同じ処理をするファイル作成

---
+++ join_jq.php
@@ -0,0 +1,153 @@
+<?php
+//エラー表示
+ini_set( 'display_errors', 1 );
+// error_reporting(E_ALL & ~E_NOTICE);
+//セッション開始
+session_start();
+//dbconnect.php読み込み(DB接続)
+require('dbconnect.php');
+//function.php読み込み(htmlspecialchars)
+require('app/functions.php');
+
+//ページリクエストがGETの場合の処理(最初の読み込み時)
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+		//join_jq.htmlを読み込み
+		include 'template/join_jq.html';
+}//ページリクエストがPOSTの場合の処理(button押下時)
+elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
+		//POSTされたJSON文字列を取り出し
+		$json = file_get_contents("php://input");
+		//JSON文字列をobjectに変換
+		$contents = json_decode($json,true);
+		//エラーメッセージ初期化
+		$message['name'] ='';
+		$message['email'] = '';
+		$message['password'] = '';
+		$message['image'] = '';
+
+		// var_dump($contents);
+	// ニックネームの記入漏れチェック
+	if(isset($contents['name']) && $contents['name'] ===''){
+		$error['name'] = 'blank';
+	}	//ニックネームの文字数チェック
+	elseif((isset($contents['name']) && mb_strlen($contents['name']) < 3)){
+		$error['name'] = 'length';
+	}elseif((isset($contents['name']) && mb_strlen($contents['name']) >12)){
+		$error['name'] = 'length';
+	}
+
+	//メールアドレスの記入漏れチェック
+	//メールアドレスが空だった場合の処理
+	if(isset($contents['email']) && $contents['email'] ===''){
+		$error['email'] = 'blank';
+	}//値が入っていたら以下のアカウント重複チェックを実行
+	else{
+		$member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+		$member->execute(array(isset($contents['email']) && $contents['email']));
+		$record = $member->fetch();
+			if($record['cnt'] > 0){
+				//エラメッセージにduplicateを設定
+				$error['email'] = 'duplicate';
+			}
+	}
+
+	//パスワードの記入漏れチェック
+	if(isset($contents['password']) && $contents['password'] ===''){
+		$error['password'] = 'blank';
+	}//パスワードの文字数チェック
+	elseif(isset($contents['password']) && $contents['password'] != '' && (strlen($contents['password']) < 4 ))
+	{
+		$error['password'] = 'length';
+	}
+
+	// 画像ファイルのエラーチェック
+	// 画像がアップロードされていた場合、下記を実行
+	// if(!empty($_FILES['image'])){
+	// 	$ext = substr($_FILES['image'], -3);
+	// 	//ファイルの拡張子チェック
+	// 	if($ext !='jpg' && $ext != 'jpeg' && $ext != 'gif' && $ext != 'png'){
+	// 	$error['image'] ='type';
+	// 	}
+	// }
+	if(isset($contents['image']) && $contents['image'] != ''){
+		$ext = substr($contents['image'], -3);
+		//ファイルの拡張子チェック
+		if($ext !='jpg' && $ext != 'jpeg' && $ext != 'gif' && $ext != 'png'){
+		$error['image'] ='type';
+		}
+	}
+
+	//ファイルアップロード
+	// if(empty($error)){
+	// 	//日付を付加したファイル名を作成し$imageに代入
+	// 	$image = date('YmdHis') . $_FILES['image']['name'];
+	// 	//保存する場所を指定してファイルをアップロード
+	// 	move_uploaded_file($_FILES['image']['tmp_name'],'member_picture/' . $image);
+	// 	//DBに保管するためセションjoinに値を保存
+	// 	$_SESSION['join'] = $contents;
+	// 	$_SESSION['join']['image'] = $image;
+	// 	//記入内容に問題がないとき、check.phpへ遷移
+	// 	header('Location: check.php');
+	// }
+	// if(empty($error)){
+	// 	//日付を付加したファイル名を作成し$imageに代入
+	// 	$image = date('YmdHis') . $contents['image']['name'];
+	// 	//保存する場所を指定してファイルをアップロード
+	// 	move_uploaded_file($contents['image']['tmp_name'],'member_picture/' . $image);
+	// 	//DBに保管するためセションjoinに値を保存
+	// 	$_SESSION['join'] = $contents;
+	// 	$_SESSION['join']['image'] = $image;
+	// 	//記入内容に問題がないとき、check.phpへ遷移
+	// 	header('Location: check.php');
+	// }
+
+	//入力フォームのエラー文表示
+	// ニックネームが記入されていない場合
+		if(isset($error['name']) && $error['name'] == 'blank'){
+				$message['name']= 'ニックネームを入力してください';
+		}//ニックネームの文字数が規定外の場合
+		elseif(isset($error['name']) && $error['name'] == 'length'){
+				$message['name']= 'ニックネームは3~12文字で記入してください';
+		}
+
+		// メールアドレスが記入されていない場合
+		if(isset($error['email']) && $error['email'] == 'blank'){
+				$message['email'] = 'メールアドレスを入力してください';
+		}// 登録済みのアドレスだった場合
+		elseif(isset($error['email']) && $error['email'] == 'duplicate'){
+				$message['email'] = '指定されたメールアドレスは、すでに登録されています';
+		}
+
+		//パスワードが4文字より少なかった場合
+		if(isset($error['password']) && $error['password'] == 'length'){
+				$message['password'] = 'パスワードは4文字以上で入力してください';
+		}// パスワード記入漏れの場合
+		elseif(isset($error['password']) && $error['password'] == 'blank'){
+				$message['password'] =  'パスワードを入力してください';
+		}
+
+		//画像ファイルの拡張子エラー文
+		if(isset($error['image']) && $error['image'] =='type'){
+				$message['image'] = '「.gif」または「.jpg」または「.png」の画像を指定してください';
+		}
+
+
+		$ary_message = array(
+			"name" => $message['name'],
+			"email" => $message['email'],
+			"password" => $message['password'],
+			"image" => $message['image']
+		);
+
+
+
+
+		//messageをJASON形式で書き出し
+		// echo $response_message;
+		print_r($ary_message);
+
+		//URLパラメータにrewriteがあれば、$contentsに$_SESSIONの内容を代入(check.phpから戻ってきた場合)
+		// if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
+		// 	$contents = $_SESSION['join'];
+		// }
+}
---
+++ js/join_jq.js
@@ -0,0 +1,119 @@
+//厳密なエラーチェック
+'use strict';
+  $(function(){
+  // //nameのチェック
+  // //formの入力時にイベントを設定
+  //   $("#name").on('input',function(e){
+
+  //     //記入されたニックネームを定数nameに代入
+  //     const name = $("#name").val();
+
+  //     //文字数によってメッセージを分岐
+  //     if(name.length > 2 && name.length < 13){
+  //       $("#message1").text("");
+  //     }else{
+  //       $("#message1").text("ニックネームは3~12文字で記入してください");
+  //     }
+  //     });
+
+  // //passwordのチェック
+  // //formの入力時にイベントを設定
+  //   $("#password").on('input',function(e){
+  //     //記入されたパスワードを定数passwordに代入
+  //     const password =  $("#password").val();
+
+  //     // passwordの入力数をコンソールでカウント
+  //     // console.log( password.length );
+
+  //     //文字数によってメッセージを分岐
+  //     if(password.length < 4 ){
+  //       $("#message3").text("パスワードは4文字以上で記入してください");
+  //     }else{
+  //       $("#message3").text("");
+  //     }
+  //   });
+
+  //   //imageの拡張子チェック
+  //   $("#image").on('change' , function(e){
+
+  //     // 画像ファイルを定数fileNameに代入
+  //     const image = $("#image").val();
+
+  //     //アップロードを許可する拡張子を設定、imageの拡張子を取得して一致しない場合の処理
+  //     if (!image.toUpperCase().match(/\.(jpg|jpeg|png|gif)$/i)){
+  //       //「ファイル選択」下にエラーメッセージを表示
+  //       $("#message4").text("「.jpg」「.jpeg」「.png」または「.gif」の画像を指定してください");
+  //     }
+  //   });
+
+  // //formVldとして、formの必須項目の入力チェック処理を設定
+  // const formVld = function (){
+  //   //formの各項目の値を定数に代入
+  //   const name = $("#name").val();
+  //   const email = $("#email").val();
+  //   const password = $("#password").val();
+
+  //   if(name === ""){
+  //       $("#message1").text("ニックネームを入力してください");
+  //   }
+  //   if(email === ""){
+  //       $("#message2").text("メールアドレスを入力してください");
+  //       }
+  //   if(password=== ""){
+  //       $("#message3").text("パスワードを入力してください");
+  //       }
+  // }
+
+  // //ボタンクリック時に必須項目の入力をチェック
+  //   $("#button").on('click',function(e){
+  //   //デフォルトのイベントキャンセル
+  //   e.preventDefault();
+  //   //formVldで設定した処理を実行
+  //   formVld();
+  //   });
+
+///////////////////非同期通信//////////////////////////////
+  $("#button").on('click',function(e){
+    //画面のリロードを停止
+    e.preventDefault();
+
+    //フォームの値を取得して変数に代入
+    // const send_data = {
+    //   name : $('#name').val(),
+    //   email : $('#email').val(),
+    //   password : $('#password').val(),
+    //   image : $('#image').val()
+    //   }
+      //ajax通信開始
+      $.ajax({
+      //タイプを指定
+      type: "POST",
+      //接続先URL
+      url: "join_jq.php",
+      //使用するHTTPメソッド
+      data:($("form").serializeArray()),
+    }) //データを受け取ったあとの処理
+    .done(function(data){
+      console.log(JSON.stringify($("form").serializeArray()))
+      //取得した合計金額を表示
+      //htmlの各idに出力
+      $('#message1').text(data.name);
+      $('#message2').text(data.email);
+      $('#message3').text(data.password);
+      $('#message4').text(data.image);
+    }).fail( (jqXHR, textStatus, errorThrown) => {
+            // Ajax通信が失敗したら発動
+            alert('Ajax通信に失敗しました。');
+            //コンソールにエラーメッセージを表示
+            console.log("ajax通信に失敗しました");
+            // HTTPステータスが取得
+            console.log("jqXHR          : " + jqXHR.status);
+            // タイムアウト、パースエラー
+            console.log("textStatus     : " + textStatus);
+            // 例外情報
+            console.log("errorThrown    : " + errorThrown.message);
+          });
+  });
+  //////////////////////////////////////////////////////////
+
+});
